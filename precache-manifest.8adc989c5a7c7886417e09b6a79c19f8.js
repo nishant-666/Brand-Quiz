19d5de2cd6b4ea99d6102ba9cd60ce9274faa0c7
nishant666 <nishants440@gmail.com>
2020-07-04 12:34:27 +0530

    Updates

---
+++ precache-manifest.8adc989c5a7c7886417e09b6a79c19f8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da37d5be12ee82934c8fe95520b09d48",
+    "url": "/Brand-Quiz/index.html"
+  },
+  {
+    "revision": "dfe7af422667614b7a2a",
+    "url": "/Brand-Quiz/static/css/2.cbe21caa.chunk.css"
+  },
+  {
+    "revision": "d49a5fd66ee47e28cd34",
+    "url": "/Brand-Quiz/static/css/main.dc0702ce.chunk.css"
+  },
+  {
+    "revision": "dfe7af422667614b7a2a",
+    "url": "/Brand-Quiz/static/js/2.4e3fed7f.chunk.js"
+  },
+  {
+    "revision": "d49a5fd66ee47e28cd34",
+    "url": "/Brand-Quiz/static/js/main.d769b267.chunk.js"
+  },
+  {
+    "revision": "4b1a53bed52edd0bd269",
+    "url": "/Brand-Quiz/static/js/runtime-main.02532794.js"
+  },
+  {
+    "revision": "a5a8d21a2720d8b357b618ceb16e7356",
+    "url": "/Brand-Quiz/static/media/background.a5a8d21a.jpg"
+  },
+  {
+    "revision": "3219419957b7d03578da90e20d8a7401",
+    "url": "/Brand-Quiz/static/media/button-sound.32194199.mp3"
+  },
+  {
+    "revision": "ee6aadae18d9da389dc08dd8d72e3721",
+    "url": "/Brand-Quiz/static/media/correct-answer.ee6aadae.mp3"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/Brand-Quiz/static/media/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/Brand-Quiz/static/media/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/Brand-Quiz/static/media/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/Brand-Quiz/static/media/materialdesignicons-webfont.a32fa1f2.eot"
+  },
+  {
+    "revision": "c7106542fc4e8075bf1bae4bf6854692",
+    "url": "/Brand-Quiz/static/media/wrong-answer.c7106542.mp3"
+  }
+]);
